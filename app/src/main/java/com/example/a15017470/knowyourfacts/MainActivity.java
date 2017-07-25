44fb8fe30d5b95112331aad64158b143e8c5796c
15017470 <15017470@myrp.edu.sg>
2017-07-25 15:05:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017470/knowyourfacts/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.a15017470.knowyourfacts;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int requestCode = 1234;
+    int randomFragmentNum;
+
+    Button btn;
+
+    ArrayList <Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.btn);
+        vPager = (ViewPager) findViewById(R.id.viewpager1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.MINUTE, 5);
+
+                Intent intent = new Intent(MainActivity.this,
+                        ScheduledNotificationReceiver.class);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        MainActivity.this, requestCode,
+                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
+
+
+            }
+        });
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+        al.add(new Frag3());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_previous) {
+            if (vPager.getCurrentItem() > 0){
+                int previousPage = vPager.getCurrentItem() - 1;
+                vPager.setCurrentItem(previousPage, true);
+            }
+        }
+
+        if (item.getItemId() == R.id.action_random) {
+            Random rnd = new Random();
+            randomFragmentNum = rnd.nextInt(150);
+            if (randomFragmentNum < 50) {
+                randomFragmentNum = 0;
+                vPager.setCurrentItem(randomFragmentNum, true);
+            } else if (randomFragmentNum < 100) {
+                randomFragmentNum = 1;
+                vPager.setCurrentItem(randomFragmentNum, true);
+            } else {
+                randomFragmentNum = 2;
+                vPager.setCurrentItem(randomFragmentNum, true);
+            }
+        }
+
+        if (item.getItemId() == R.id.action_next) {
+            int max = vPager.getChildCount();
+            if (vPager.getCurrentItem() < max-1){
+                int nextPage = vPager.getCurrentItem() + 1;
+                vPager.setCurrentItem(nextPage, true);
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
